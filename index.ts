02134a84433c20ca57bf90c3751e852aabd8dfd5
Andrey Acosta  R <andreyacost24ra18@gmail.com>
2023-06-11 19:56:15 -0600

    first commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express from "express"
+
+const app = express()
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
+
+const PUERTO = process.env.PORT || 3000;
+
+app.listen(PUERTO, () => {
+  console.log(`The server is listening on the port ${PUERTO}...`);
+});
