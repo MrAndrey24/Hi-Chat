c70c62dd77db52e545aa4e54e38af271b821c7d5
Andrey Acosta  R <andreyacostar@gmail.com>
2023-07-03 17:44:29 -0600

    express-validator

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from 'express';
-import { body } from "express-validator";
+import { body, validationResult } from "express-validator";
 import * as userServices from "../services/userServices"
 import { User } from '../models/user';
 // import { registerSchema } from '../schema/register-schema';
-import { validateRequestSchema } from '../middleware/validate-rquest-schema';
+// import { validateRequestSchema } from '../middleware/validate-rquest-schema';
 
 const router = express.Router();
 
@@ -13,7 +13,11 @@ router.get("/",(_req, res) => {
 
 router.post("/",[  body("name").isString(),
 body("email").isEmail(),
-body("password").isString().isLength({ min: 5}),],validateRequestSchema,(req: Request, res: Response) => {
+body("password").isString().isLength({ min: 5})],(req: Request, res: Response) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
 
     const {name, email, password} = req.body as User
     const newUserEntry = userServices.addUser({
@@ -23,6 +27,7 @@ body("password").isString().isLength({ min: 5}),],validateRequestSchema,(req: Re
     })
         
     res.json(newUserEntry)
+    return
 })
 
 
--- src/schema/register-schema.ts
+++
@@ -1,9 +0,0 @@
-// import { body } from "express-validator";
-
-// const schema = [
-//     body("name").isString(),
-//     body("email").isEmail(),
-//     body("password").isString().isLength({ min: 5}),
-// ]
-
-// export { schema as registerSchema };
