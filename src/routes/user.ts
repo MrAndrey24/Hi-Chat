b0b519272d48918f99ba6340c0019360ac18f541
Andrey Acosta  R <andreyacostar@gmail.com>
2023-07-04 16:08:56 -0600

    method get,put,post

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,28 +1,62 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from "express-validator";
+import { body, param, validationResult } from "express-validator";
 import * as userServices from "../services/userServices"
 import { User } from '../models/user';
 
 const router = express.Router();
 
-router.get("/",(_req, res) => {
-    res.send(userServices.getUser())
+router.get("/", async (_req, res) => {
+    const users = await userServices.getUser()
+    res.send(users)
 })
 
-router.post("/",[  body("name").isString(),body("email").isEmail(),body("password").isString().isLength({ min: 5})],(req: Request, res: Response) => {
+router.get("/:id", param("id").isNumeric(), async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const userId: number = parseInt(req.params.id);
+    const user = await userServices.getUserById(userId);
+    
+    if (user) {
+        res.send(user);
+    } else {
+        res.status(404).json({ message: "User not found" })
+    }
+    return
+})
+
+
+router.post("/",[body("name").isString(),body("email").isEmail(),body("password").isString().isLength({ min: 5})], async (req: Request, res: Response) => {
     const errors = validationResult(req)
     if(!errors.isEmpty()){
-        return res.status(400).json({ errors: errors.array() });
+        return res.status(400).json({ errors: errors.array() })
     }
 
     const {name, email, password} = req.body as User
-    const newUserEntry = userServices.addUser({
+    const newUserEntry = await userServices.addUser({
     name,
     email,
     password
     })
         
     res.json(newUserEntry)
+    return
+})
+
+router.put("/:id",[param("id").isNumeric(), body("name").optional().isString(), body("email").optional().isEmail(), body("password").optional().isString().isLength({ min: 5})], async (req: Request, res: Response) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const userId: number = parseInt(req.params.id)
+    const {name, email, password} = req.body as User
+    const userData = await userServices.updateUser(userId, {name, email, password})
+    if(userData){
+    res.json(userData)
+    }
     return
 })
 
--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -5,6 +5,11 @@ const users: User[] = userData as User[];
 
 export const getUser = (): User[] => users
 
+export const getUserById = (id: number): User | null => {
+    const user = users.find(user => user.id === id);
+    return user ? user : null
+}
+
 export const addUser = (newUserEntry: NewUserEntry): User => {
     const newUser = {
         id: Math.max(...userData.map(user => user.id)) + 1,
@@ -13,4 +18,16 @@ export const addUser = (newUserEntry: NewUserEntry): User => {
 
     userData.push(newUser);
     return newUser
+}
+
+export const updateUser = (userId: number, updatedUser: Partial<User>): User | null => {
+    const userIndex = users.findIndex(user => user.id === userId);
+    if(userIndex !== -1) {
+        const originalUser = userData[userIndex];
+        const updatedUserData = { ...originalUser, ...updatedUser}
+        userData[userIndex] = updatedUserData
+        return updatedUserData
+    }
+
+    return null
 }
