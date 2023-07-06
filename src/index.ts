118178c13d7b71ca526813c9f87fde917455b030
Axel Jimenez <ajimenezq1@ucenfotec.ac.cr>
2023-07-06 00:53:19 -0600

    Add detection of different new connections to server

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,51 @@
-import express from "express"
-import dotenv from "dotenv"
-import userRouter from "./routes/user"
-import mongoose from "mongoose"
-import path from "path"
-import io from "socket.io"
+import express from "express";
+import dotenv from "dotenv";
+import userRouter from "./routes/user";
+import mongoose from "mongoose";
+import path from "path";
+import { Server } from "socket.io"
 
-const app = express()
-app.use(express.json())
+//Creating socket.io server
+const app = express();
+const server = require("http").createServer(app);
+const io = new Server(server);
 
+//Detects new connection and prints msg
+io.on('connection', (socket) => {
+  console.log('New socket')
+
+  //On disconnect, print msg
+  socket.on('disconnect', () => {
+    console.log('Socket disconnected');
+  });
+});
+
+app.use(express.json());
+
+//Shows index.html file when starting server
 app.get("/", (_req, res) => {
-  res.sendFile(path.join(__dirname, '/www', 'index.html'))
-})
+  res.sendFile(path.join(__dirname, '/www', 'index.html'));
+});
 
 //Routers
-app.use("/api/v1/users", userRouter)
+app.use("/api/v1/users", userRouter);
 
-dotenv.config()
+dotenv.config();
 
 const PUERTO = process.env.PORT || 3000;
-const url = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@proyectocomponentes.jga5vk8.mongodb.net/?retryWrites=true&w=majority`
+const url = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@proyectocomponentes.jga5vk8.mongodb.net/?retryWrites=true&w=majority`;
 
 const connectMongoDb = async () => {
   try{
-    await mongoose.connect(url)
-    console.log("Successfully connected to MongoDB")
+    await mongoose.connect(url);
+    console.log("Successfully connected to MongoDB");
   }catch(e){
-    console.log("Failed to connect to MongoDB: " + e)
+    console.log("Failed to connect to MongoDB: " + e);
   }
-}
+};
 
-connectMongoDb()
+connectMongoDb();
 
-app.listen(PUERTO, () => {
+server.listen(PUERTO, () => {
   console.log(`The server is listening on the port ${PUERTO}...`);
 });
