d7fb741471c4512f9c87c38dae5468f45f980876
Axel Jimenez <ajimenezq1@ucenfotec.ac.cr>
2023-08-15 17:02:57 -0600

    Add redirection of css links to files

--- dist/index.js
+++ dist/index.js
@@ -82,17 +82,23 @@ app.get('/register.js', (_req, res) => {
     res.sendFile(path_1.default.join(__dirname, '../dist', '/www', '/ts', 'register.js'));
 });
 app.get('/index.html', (_req, res) => {
-    res.sendFile(path_1.default.join(__dirname, '../dist', '/www', 'index.html'));
-});
-// When localhost:3000/chat, locate and send html files to the client
-app.get('/www/', (_req, res) => {
     res.sendFile(path_1.default.join(__dirname, '../src', '/www', 'index.html'));
 });
+// When localhost:3000/chat, locate and send html files to the client
 app.get('/', (_req, res) => {
     res.sendFile(path_1.default.join(__dirname, '../src', '/www', 'login.html'));
 });
 app.get('/www/', (_req, res) => {
     res.sendFile(path_1.default.join(__dirname, '../src', '/www', 'register.html'));
+});
+app.get('/index.css', (_req, res) => {
+    res.sendFile(path_1.default.join(__dirname, '../src', '/www', '/css', 'index.css'));
+});
+app.get('/login.css', (_req, res) => {
+    res.sendFile(path_1.default.join(__dirname, '../src', '/www', '/css', 'login.css'));
+});
+app.get('/register.css', (_req, res) => {
+    res.sendFile(path_1.default.join(__dirname, '../src', '/www', '/css', 'register.css'));
 });
 app.use(express_1.default.json());
 //Routers
